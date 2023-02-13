6bcd6ad646e940ec672cc956b40eafd094560bde
Abhishek Chaudhary <cabhishek745@gmail.com>
2023-02-13 23:38:12 +0530

    added basic UI - html and js

---
+++ views/script.js
@@ -0,0 +1,14 @@
+// const socket = io()
+console.log("SDAWDAWDAWDAWDAWDAWDADWA");
+// const sendButton = document.getElementById("sendBtn");
+// const messageText = document.getElementById("messageText");
+// const chatContainer = document.getElementById("chatContainer");
+
+// sendButton.addEventListener('click', sendMessage);
+
+// function sendMessage() {
+//     console.log("saddawddawdad");
+//     const message = messageText.value;
+//     chatContainer.innerHTML = chatContainer.innerHTML += message + "<br>";
+//     messageText.value = '';
+// }
